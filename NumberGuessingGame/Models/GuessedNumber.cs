0d0bdd2d551b4c038a823eaa0d45423ba4febc2e
Your Name Here <fj222dr@student.lnu.se>
2013-11-18 18:44:30 +0100

    Skapat model klasserna för att uppfylla testfallen

---
+++ NumberGuessingGame/Models/GuessedNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NumberGuessingGame.Models
+{
+    public struct GuessedNumber
+    {
+        public int? Number;
+        public Outcome Outcome;
+
+    }
+}
---
+++ NumberGuessingGame/Models/SecretNumber.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NumberGuessingGame.Models
+{
+    public class SecretNumber
+    {
+
+        private List<GuessedNumber> _guessedNumbers;
+
+        private GuessedNumber _lastGuessedNumber;
+
+        private int? _number;
+
+        public const int MaxNumberOfGuesses = 7;
+
+        public bool CanMakeGuess
+        {
+            get 
+            {
+                return this.Count < MaxNumberOfGuesses && this.LastGuessedNumber.Outcome != Outcome.Right;
+            }
+        }
+
+        public int Count 
+        {
+            get
+            {
+                return this._guessedNumbers.Count();
+            }
+        }
+
+        public IList<GuessedNumber> GuessedNumbers
+        {
+            get
+            {
+                return this._guessedNumbers.AsReadOnly();
+            }
+        }
+
+        public GuessedNumber LastGuessedNumber
+        {
+            get 
+            {
+                return _lastGuessedNumber;
+            }
+        }
+
+        public int? Number
+        {
+            get
+            {
+                return this.CanMakeGuess ? null : this._number;
+            }
+            private set
+            {
+                this._number = value;
+            }
+        }
+
+        public void Initialize() 
+        {
+            this._guessedNumbers.Clear();
+            Random random = new Random();
+            this.Number = random.Next(1, 100);
+
+            this._lastGuessedNumber = new GuessedNumber();
+            this._lastGuessedNumber.Outcome = Outcome.Indefinite;
+        }
+
+        public Outcome MakeGuess(int guess)
+        {
+            if (guess > 100 || guess < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            else if (!this.CanMakeGuess)
+            {
+                return Outcome.NoMoreGuesses;
+            }
+            else if (this._guessedNumbers.Exists(x => x.Number == guess))
+            {
+                return Outcome.OldGuess;
+            }
+
+            if (guess > _number)
+            {
+                this._lastGuessedNumber.Outcome = Outcome.High;
+            }
+            else if (guess < _number)
+            {
+                this._lastGuessedNumber.Outcome = Outcome.Low;
+            }
+            else
+            {
+                this._lastGuessedNumber.Outcome = Outcome.Right;
+            }
+
+            this._lastGuessedNumber.Number = guess;
+            this._guessedNumbers.Add(this._lastGuessedNumber);
+            return this._lastGuessedNumber.Outcome;
+        }
+
+        public SecretNumber() 
+        {
+            this._guessedNumbers = new List<GuessedNumber>(MaxNumberOfGuesses);
+            this.Initialize();
+        }
+    }
+}
