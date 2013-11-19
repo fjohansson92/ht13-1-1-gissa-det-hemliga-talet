5b5ac102d47e85fd281e276f32d8705e89e053eb
Your Name Here <fj222dr@student.lnu.se>
2013-11-19 17:55:04 +0100

    Gjort så man kan ange en gissning

---
+++ NumberGuessingGame/Controllers/NumberGuessingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NumberGuessingGame.ViewModels;
+
+namespace NumberGuessingGame.Controllers
+{
+    public class NumberGuessingController : Controller
+    {
+        //
+        // GET: /NumberGuessing/
+
+        public ActionResult Index()
+        {
+            var model = new GuessingIndexViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(GuessingIndexViewModel guessingIndexViewModel)
+        {
+            return View(guessingIndexViewModel);
+        }
+        
+    }
+}
---
+++ NumberGuessingGame/ViewModels/GuessingIndexViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NumberGuessingGame.ViewModels
+{
+    public class GuessingIndexViewModel
+    {
+        [Required(ErrorMessage = "Du måste göra en gissning.")]
+        [Range(1, 100, ErrorMessage = "Talet måste vara mellan 1 och 100.")]
+        public int Guess { get; set; }
+    }
+}
